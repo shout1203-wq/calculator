85d86bbd965dcb4a4a4b1d9fb358fa46fd1e387a
developer1 <dev1@email.com>
2026-06-15 22:06:03 +0900

    Add icon.png file and push buton with handler function in main.py

--- main.py
+++ main.py
@@ -1,33 +1,37 @@
 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import (QApplication,QWidget,QPushButton,QVBoxLayout,
+                             QMessageBox)
+from PyQt5.QtGui import QIcon
 
-print("1. 프로그램 시작")
 
 class Calculator(QWidget):
     
     def __init__(self):
-        print("2. Calculator __init__ 진입")
         super().__init__()
         self.initUI()
         
     def initUI(self):
-        print("3. initUI 실행됨")
+        self.btn1=QPushButton('Message',self)
+        self.btn1.clicked.connect(self.activateMessage)
+        
+        vbox=QVBoxLayout()
+        vbox.addStretch(1)
+        vbox.addWidget(self.btn1)
+        vbox.addStretch(1)
+        
+        self.setLayout(vbox)
+        
         self.setWindowTitle('Calculator')
-        self.resize(256, 256)
+        self.setWindowIcon(QIcon('icon.png'))
+        self.resize(256,256)
         self.show()
+        
+    def activateMessage(self):
+        QMessageBox.information(self,"information","Button clicked!")
         
         
 if __name__ == '__main__':
-    print("4. main 시작")
-    
-    app = QApplication(sys.argv)
-    print("5. QApplication 생성 완료")
-    
-    view = Calculator()
-    print("6. Calculator 객체 생성 완료")
-    
-    print("7. app.exec_() 진입 직전")
+    app=QApplication(sys.argv)
+    view=Calculator()
     sys.exit(app.exec_())
-    
-    print("8. 여기 찍히면 비정상 (실행 안되는게 정상)")
